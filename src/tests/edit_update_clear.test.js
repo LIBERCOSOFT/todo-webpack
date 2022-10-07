8507d300102aef305530fd4dd66511b328186564
LIBERCOSOFT <akinspreciousola@gmail.com>
2022-10-07 10:43:03 +0100

    Add more test cases

--- src/tests/edit_update_clear.test.js
+++ src/tests/edit_update_clear.test.js
@@ -3,9 +3,29 @@
  */
 
 import addItemToDom from '../modules/addItemToDom.js';
-import { handleEdit, handleCheckbox, deleteAllCompleted } from '../modules/eventListeners.js';
+import {
+  handleEdit, handleCheckbox, deleteAllCompleted,
+} from '../modules/eventListeners.js';
 
 describe('successful edit of task description', () => {
+  it('checks the data type given to handlEdit function', () => {
+    document.body.innerHTML = '<ul id="items__container"></ul>';
+    const newItem = {
+      description: 'value one',
+      completed: false,
+      index: 1,
+    };
+    const e = {
+      target: {
+        parentElement: { parentElement: { id: newItem.index } },
+        innerText: 'another two',
+      },
+    };
+    addItemToDom(newItem);
+    expect(e).toBeInstanceOf(Object);
+    handleEdit(e);
+  });
+
   it('updates the task description on the dom', () => {
     document.body.innerHTML = '<ul id="items__container"></ul>';
     const newItem = {
@@ -27,7 +47,26 @@ describe('successful edit of task description', () => {
 });
 
 describe('successful marking an item completed', () => {
-  it('add a class of completed to item', () => {
+  it('checks the data type given to handleCheckbox function', () => {
+    document.body.innerHTML = '<ul id="items__container"></ul>';
+    const newItem = {
+      description: 'value one',
+      completed: false,
+      index: 1,
+    };
+    const e = {
+      target: {
+        parentElement: {
+          parentElement: { id: newItem.index },
+        },
+      },
+    };
+    addItemToDom(newItem);
+    expect(e).toBeInstanceOf(Object);
+    handleCheckbox(e);
+  });
+
+  it('add a class of completed to completed item', () => {
     document.body.innerHTML = '<ul id="items__container"></ul>';
     const newItem = {
       description: 'value one',
@@ -49,6 +88,23 @@ describe('successful marking an item completed', () => {
 });
 
 describe('delete all item', () => {
+  it('checks if the deleteAllCompleted has an argument', () => {
+    document.body.innerHTML = '<ul id="items__container"></ul>';
+    const newItem = {
+      description: 'value',
+      completed: true,
+      index: 1,
+    };
+
+    addItemToDom(newItem);
+    function deleteAllCompleted(...args) {
+      const { length } = [...args];
+      return length === 0;
+    }
+
+    expect(deleteAllCompleted()).toBeTruthy();
+  });
+
   it('delete all completed item from the list', () => {
     document.body.innerHTML = '<ul id="items__container"></ul>';
     const newItem = {
